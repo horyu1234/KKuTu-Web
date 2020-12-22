2789c284c3b67d0fb31e8a22ca3ed8a412d7fccc
horyu1234 <horyu1234@naver.com>
2020-12-22 22:28:55 +0900

    Kotlin 버전 업데이트

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.3.4.RELEASE"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
-    kotlin("jvm") version "1.3.72"
-    kotlin("plugin.spring") version "1.3.72"
+    kotlin("jvm") version "1.4.21"
+    kotlin("plugin.spring") version "1.4.21"
 
     id("com.github.node-gradle.node") version "2.2.4"
 }
